622d84e2c1671f2788641ddab10e55ea0581d049
marcio.ide <marcio.ide@livegroup.com.br>
2015-03-19 14:46:43 -0300

    status compra

---
+++ LM.Core.Domain/StatusCompra.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LM.Core.Domain
+{
+    public enum StatusCompra
+    {
+        ItemAComprar = 0,
+        Comprado = 1,
+        AgoraNao = 2,
+        NaoEncontrado = 3,
+        ItemSubstituido = 4
+    }
+}
