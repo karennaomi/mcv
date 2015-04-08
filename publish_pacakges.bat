42ebb89327144e7f4cc59808f5a130aec81b6ab6
marcio.ide <marcio.ide@livegroup.com.br>
2015-04-08 18:10:26 -0300

    bump version

--- publish_pacakges.bat
+++ publish_pacakges.bat
@@ -6,10 +6,6 @@ move LM.Core.Application\NugetReleases\*.nupkg LM.Core.Application\NugetReleases
 nuget pack LM.Core.Application\LM.Core.Application.csproj -Prop Configuration=Release -OutputDirectory LM.Core.Application\NugetReleases
 nuget push -Source http://nuget.smv.br -ApiKey 648CA9AC80FE4CAFA2E581E0FEADCC26 LM.Core.Application\NugetReleases\*.nupkg
 
-move LM.Core.Repository\NugetReleases\*.nupkg LM.Core.Repository\NugetReleases\old
-nuget pack LM.Core.Repository\LM.Core.Repository.csproj -Prop Configuration=Release -OutputDirectory LM.Core.Repository\NugetReleases
-nuget push -Source http://nuget.smv.br -ApiKey 648CA9AC80FE4CAFA2E581E0FEADCC26 LM.Core.Repository\NugetReleases\*.nupkg
-
 move LM.Core.RepositorioEF\NugetReleases\*.nupkg LM.Core.RepositorioEF\NugetReleases\old
 nuget pack LM.Core.RepositorioEF\LM.Core.RepositorioEF.csproj -Prop Configuration=Release -OutputDirectory LM.Core.RepositorioEF\NugetReleases
 nuget push -Source http://nuget.smv.br -ApiKey 648CA9AC80FE4CAFA2E581E0FEADCC26 LM.Core.RepositorioEF\NugetReleases\*.nupkg
