88906bc5f4dc8b37eabaf5ff66ea858eb14fb29b
marcio.ide <marcio.ide@livegroup.com.br>
2015-04-10 16:59:53 -0300

    iniciando propriedades do item do pedido

--- LM.Core.Domain/PedidoItem.cs
+++ LM.Core.Domain/PedidoItem.cs
@@ -4,15 +4,20 @@ namespace LM.Core.Domain
 {
     public class PedidoItem
     {
+        public PedidoItem()
+        {
+            DataInclusao = DataAlteracao = DateTime.Now;
+            Status = StatusPedido.Pendente;
+        }
+        
         public long Id { get; set; }
         public DateTime? DataInclusao { get; set; }
         public DateTime? DataAlteracao { get; set; }
-        public virtual Produto Produto { get; set; }
-
         public decimal Quantidade { get; set; }
         public StatusPedido Status { get; set; }
         public DateTime Data { get; set; }
 
+        public virtual Produto Produto { get; set; }
         public virtual PontoDemanda PontoDemanda { get; set; }
         public virtual Integrante Integrante { get; set; }
     }
