830f0082549174e53314a091fa8430044af8df32
marcio.ide <marcio.ide@livegroup.com.br>
2015-05-14 16:04:18 -0300

    listar sugestao por categoria

--- LM.Core.Application/CompraAplicacao.cs
+++ LM.Core.Application/CompraAplicacao.cs
@@ -10,6 +10,7 @@ namespace LM.Core.Application
     {
         IList<Categoria> ListarSecoes(long pontoDemandaId);
         IEnumerable<IItem> ListarSugestao(long pontoDemandaId);
+        IEnumerable<IItem> ListarSugestaoPorCategoria(long pontoDemandaI, int categoriaId);
         Compra Obter(long pontoDemandaId, long id);
         Compra Criar(Compra compra);
     }
@@ -31,6 +32,12 @@ namespace LM.Core.Application
             IEnumerable<IItem> pedidos = _appPedido.ListarItensPorStatus(pontoDemandaId, StatusPedido.Pendente);
             IEnumerable<IItem> itens = _appLista.ListarItens(pontoDemandaId).Where(i => i.EhSugestaoDeCompra);
             return pedidos.Union(itens).OrderBy(i => i.Produto.Categorias.First().CategoriaPai.Nome);
+        }
+
+        public IEnumerable<IItem> ListarSugestaoPorCategoria(long pontoDemandaId, int categoriaId)
+        {
+            if (categoriaId == 0) return _appPedido.ListarItensPorStatus(pontoDemandaId, StatusPedido.Pendente);
+            return _appLista.ListarItens(pontoDemandaId).Where(i => i.EhSugestaoDeCompra && i.Produto.Categorias.Any(c => c.CategoriaPai.Id == categoriaId));
         }
 
         public IList<Categoria> ListarSecoes(long pontoDemandaId)
