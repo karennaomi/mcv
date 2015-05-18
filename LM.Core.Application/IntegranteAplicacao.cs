6335952179302ece7a66045142a485ab7f219416
marcio.ide <marcio.ide@livegroup.com.br>
2015-05-18 12:26:53 -0300

    nao verficar email existente quando o campo email for vazio

--- LM.Core.Application/IntegranteAplicacao.cs
+++ LM.Core.Application/IntegranteAplicacao.cs
@@ -42,7 +42,7 @@ namespace LM.Core.Application
         {
             var integranteToUpdate = Obter(pontoDemandaId, integrante.Id);
             if(integrante.EhUsuarioDoSistema()) throw new ApplicationException("Não pode atualizar um usuário do sistema.");
-            if (integranteToUpdate.Email != integrante.Email) _repositorio.VerificarSeEmailJaExiste(integrante.Email);
+            if (!string.IsNullOrWhiteSpace(integrante.Email) && integranteToUpdate.Email != integrante.Email) _repositorio.VerificarSeEmailJaExiste(integrante.Email);
             integranteToUpdate.Atualizar(integrante);
             _repositorio.Salvar();
             return integranteToUpdate;
