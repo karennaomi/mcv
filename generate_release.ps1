e17b8cdd8dcfed9b58f66f79552439b3a56dc53c
marcio.ide <marcio.ide@livegroup.com.br>
2015-07-31 10:05:35 -0300

    update generate release script

--- generate_release.ps1
+++ generate_release.ps1
@@ -6,10 +6,15 @@ $homJob="LM_Core"
 $devToken="D10C42FA01A2435EA3BF92D8D2C5C4FB"
 $homToken="D10C42FA01A2435EA3BF92D8D2C5C4FB"
 
-
 $latestTag = Invoke-Expression 'git describe --abbrev=0 --tags 2>$null'
 if($latestTag -eq $null) {
 	$latestTag = "v0.0.0"
+}
+
+function CreateVersionFile($tag) {
+	New-Item $projFolder\version.txt -type file -force -value $tag
+	git add $projFolder\version.txt
+	git commit -am "new version $tag"
 }
 
 function PushTag($tag) {
@@ -37,9 +42,10 @@ function TriggerBuildWithTag($tag) {
 	(New-Object System.Net.WebClient).DownloadString("$url");
 }
 
-function Publish($tagVersion) {
-	PushTag $tagVersion
-	TriggerBuildWithTag $tagVersion
+function Publish($tag) {
+	CreateVersionFile $tag
+	PushTag $tag
+	TriggerBuildWithTag $tag
 }
 
 if($latestTag -match 'v(?<major>\d+)\.?(?<minor>\d+)\.(?<patch>\d)+') {
