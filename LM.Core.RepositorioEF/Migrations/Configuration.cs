01d66ed330f09d99ceff0126185147510a479f20
marcio.ide <marcio.ide@livegroup.com.br>
2015-08-04 16:03:02 -0300

    migration configuration public

--- LM.Core.RepositorioEF/Migrations/Configuration.cs
+++ LM.Core.RepositorioEF/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace LM.Core.RepositorioEF.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<LM.Core.RepositorioEF.ContextoEF>
+    public sealed class Configuration : DbMigrationsConfiguration<LM.Core.RepositorioEF.ContextoEF>
     {
         public Configuration()
         {
