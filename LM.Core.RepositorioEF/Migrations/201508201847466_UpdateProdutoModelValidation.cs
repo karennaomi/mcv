b2d8c42ad971cd17afffa1dd2289983fa5b61dc5
marcio.ide <marcio.ide@livegroup.com.br>
2015-08-20 15:50:34 -0300

    migration para ajuste no modelo de Produto

---
+++ LM.Core.RepositorioEF/Migrations/201508201847466_UpdateProdutoModelValidation.cs
@@ -0,0 +1,20 @@
+namespace LM.Core.RepositorioEF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateProdutoModelValidation : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.TB_PRODUTO", "CD_PRODUTO_EAN", c => c.String(maxLength: 13));
+            AlterColumn("dbo.TB_PRODUTO_MASTER_DATA", "NM_PRODUTO_COMPLETO", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.TB_PRODUTO_MASTER_DATA", "NM_PRODUTO_COMPLETO", c => c.String());
+            AlterColumn("dbo.TB_PRODUTO", "CD_PRODUTO_EAN", c => c.String(nullable: false, maxLength: 13));
+        }
+    }
+}
