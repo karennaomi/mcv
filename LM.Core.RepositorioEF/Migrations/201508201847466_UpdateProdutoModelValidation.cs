b33a2680d83f44fdd7cc7252c5f7a637cc5637d5
marcio.ide <marcio.ide@livegroup.com.br>
2015-08-20 16:36:39 -0300

    desabilitando acoes do migration por causa do full text search

--- LM.Core.RepositorioEF/Migrations/201508201847466_UpdateProdutoModelValidation.cs
+++ LM.Core.RepositorioEF/Migrations/201508201847466_UpdateProdutoModelValidation.cs
@@ -7,14 +7,15 @@ namespace LM.Core.RepositorioEF.Migrations
     {
         public override void Up()
         {
-            AlterColumn("dbo.TB_PRODUTO", "CD_PRODUTO_EAN", c => c.String(maxLength: 13));
-            AlterColumn("dbo.TB_PRODUTO_MASTER_DATA", "NM_PRODUTO_COMPLETO", c => c.String(nullable: false));
+            //desabilitando alterações pq as colunas estao com indices de full text search
+            //AlterColumn("dbo.TB_PRODUTO", "CD_PRODUTO_EAN", c => c.String(maxLength: 13));
+            //AlterColumn("dbo.TB_PRODUTO_MASTER_DATA", "NM_PRODUTO_COMPLETO", c => c.String(nullable: false));
         }
         
         public override void Down()
         {
-            AlterColumn("dbo.TB_PRODUTO_MASTER_DATA", "NM_PRODUTO_COMPLETO", c => c.String());
-            AlterColumn("dbo.TB_PRODUTO", "CD_PRODUTO_EAN", c => c.String(nullable: false, maxLength: 13));
+            //AlterColumn("dbo.TB_PRODUTO_MASTER_DATA", "NM_PRODUTO_COMPLETO", c => c.String());
+            //AlterColumn("dbo.TB_PRODUTO", "CD_PRODUTO_EAN", c => c.String(nullable: false, maxLength: 13));
         }
     }
 }
