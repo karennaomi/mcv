5e29f55cb7e7653b202a4a19506e9f9228049504
marcio.ide <marcio.ide@livegroup.com.br>
2015-08-21 18:00:43 -0300

    checando null nas procedures

--- LM.Core.RepositorioEF/Procedures.cs
+++ LM.Core.RepositorioEF/Procedures.cs
@@ -1,4 +1,5 @@
-﻿using LM.Core.Domain.Repositorio;
+﻿using System;
+using LM.Core.Domain.Repositorio;
 using System.Data.SqlClient;
 
 namespace LM.Core.RepositorioEF
@@ -15,32 +16,29 @@ namespace LM.Core.RepositorioEF
             _contexto = contexto;
         }
 
+        public void RecalcularSugestao(long pontoDemandaId, int? produtoId = null)
+        {
+            var pontoDemandaIdParam = new SqlParameter("@ID_PONTO_REAL_DEMANDA", pontoDemandaId);
+            var produtoIdParam = produtoId.HasValue ? new SqlParameter("@ID_PRODUTO", produtoId.Value) : new SqlParameter("@ID_PRODUTO", DBNull.Value);
+            _contexto.Database.ExecuteSqlCommand("SP_SPSS_CALCULO_SUGESTAO_PONTO_DEMANDA @ID_PONTO_REAL_DEMANDA, @ID_PRODUTO", pontoDemandaIdParam, produtoIdParam);
+        }
+
         public void LancarEstoque(long pontoDemandaId, int origem, int? produtoId, decimal? quantidade, long integranteId)
         {
             var pontoDemandaIdParam = new SqlParameter("@IDPReD", pontoDemandaId);
             var origemParam = new SqlParameter("@IDOrigemLancamentoEstoque", origem);
-            var produtoIdParam = new SqlParameter("@IDProduto", produtoId);
-            var quantidadeParam = new SqlParameter("@QtLancada", quantidade);
+            var produtoIdParam = produtoId.HasValue ? new SqlParameter("@IDProduto", produtoId) : new SqlParameter("@IDProduto", DBNull.Value);
+            var quantidadeParam = quantidade.HasValue ? new SqlParameter("@QtLancada", quantidade) : new SqlParameter("@QtLancada", DBNull.Value);
             var integranteIdParam = new SqlParameter("@IDIntegrante", integranteId);
             _contexto.Database.ExecuteSqlCommand("SP_APP_EFETUA_LANCAMENTO_ESTOQUE @IDPReD, @IDOrigemLancamentoEstoque, @IDProduto, @QtLancada, @IDIntegrante", pontoDemandaIdParam, origemParam,
                 produtoIdParam, quantidadeParam, integranteIdParam);
         }
 
-
         public void InserirProdutoNaFila(string ean, string nome)
         {
-            var eanParam = new SqlParameter("@EAN", ean);
-            var produtoNomeParam = new SqlParameter("@NomeProduto", nome);
+            var eanParam = string.IsNullOrEmpty(ean) ? new SqlParameter("@EAN", DBNull.Value) : new SqlParameter("@EAN", ean);
+            var produtoNomeParam = string.IsNullOrEmpty(nome) ? new SqlParameter("@NomeProduto", DBNull.Value) : new SqlParameter("@NomeProduto", nome);
             _contexto.Database.ExecuteSqlCommand("SP_INSERE_PRODUTO_NOVO_FILA @EAN, @NomeProduto", eanParam, produtoNomeParam);
-        }
-
-        public void RecalcularSugestao(long pontoDemandaId, int? produtoId = null)
-        {
-            var pontoDemandaIdParam = new SqlParameter("@ID_PONTO_REAL_DEMANDA", pontoDemandaId);
-            var produtoIdParam = new SqlParameter("@ID_PRODUTO", produtoId);
-            _contexto.Database.ExecuteSqlCommand(
-                "SP_SPSS_CALCULO_SUGESTAO_PONTO_DEMANDA @ID_PONTO_REAL_DEMANDA, @ID_PRODUTO", pontoDemandaIdParam,
-                produtoIdParam);
         }
     }
 }
--- LM.Core.RepositorioEF/Properties/AssemblyInfo.cs
+++ LM.Core.RepositorioEF/Properties/AssemblyInfo.cs
@@ -41,7 +41,7 @@ namespace LM.Core.RepositorioEF.Properties
 {
     internal struct Version
     {
-        public const string CurrentVersion = "1.12.19";
+        public const string CurrentVersion = "1.12.20";
     }
 }
 
