1cc71a20a344255aeb0fe8fe78b69ea1ba31752c
marcio.ide <marcio.ide@livegroup.com.br>
2015-09-11 16:05:06 -0300

    ajuste no rest service para realizar requisicoes nao genericas

--- LM.Core.Application/RestServiceWithRestSharp.cs
+++ LM.Core.Application/RestServiceWithRestSharp.cs
@@ -23,20 +23,34 @@ namespace LM.Core.Application
             return _pushServiceClient.ExecutePostTaskAsync(request);
         }
 
+        public void Get(string endpoint)
+        {
+            var request = new RestRequest(endpoint);
+            var response = _pushServiceClient.Get(request);
+            CheckResponse(response);
+        }
+
         public T Get<T>(string endpoint) where T : new()
         {
             var request = new RestRequest(endpoint);
             var response = _pushServiceClient.Get<T>(request);
+            CheckResponse(response);
+            return response.Data;
+        }
+
+        private static void CheckResponse(IRestResponse response)
+        {
             switch (response.ResponseStatus)
             {
                 case ResponseStatus.Error:
                 case ResponseStatus.Aborted:
-                    if (response.StatusCode == HttpStatusCode.NotFound) throw new ObjetoNaoEncontradoException("O recurso buscado não foi encontrado");
+                    if (response.StatusCode == HttpStatusCode.NotFound)
+                        throw new ObjetoNaoEncontradoException("O recurso buscado não foi encontrado");
                     throw new ApplicationException("Ocorreu um erro na requisição com o serviço.");
                 case ResponseStatus.TimedOut:
                     throw new ApplicationException("O tempo de execução do serviço esgotou.");
                 default:
-                    return response.Data;
+                    return;
             }
         }
 
